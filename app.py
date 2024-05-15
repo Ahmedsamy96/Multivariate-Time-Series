3fcaab0a8f6b219bf49537a7d7f76b2dbdb1aeeb
Ahmed Samy <Ahmed.samy18296@gmail.com>
2024-05-15 19:15:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,17 @@ from prophet import Prophet
 from prophet.plot import add_changepoints_to_plot
 import streamlit as st
 
-@st.cache_data
+@st.cache_data  # 👈 Add the caching decorator
+def load_data(url):
+    df = pd.read_csv(url.content)
+    return df
+
 data_url= r"https://github.com/Ahmedsamy96/Multivariate-Time-Series/blob/main/IOT-temp.csv"
 data_response = requests.get(data_url)
 data_response.raise_for_status()  # Check if download was successful
-df = pd.read_csv(data_response.content)
+df = load_data(data_response)
+#df = pd.read_csv(data_response.content)
+
 df.drop('room_id/id', axis=1, inplace=True)
 
 # Change column names to understand easily
