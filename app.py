3580a784f2d586af19d8b77892676c3f5eaf4088
Ahmed Samy <Ahmed.samy18296@gmail.com>
2024-05-15 19:57:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ import streamlit as st
 
 
 data_url= r"https://github.com/Ahmedsamy96/Multivariate-Time-Series/blob/main/IOT-temp.csv"
-#data_response = requests.get(data_url)
+data_response = requests.get(data_url)
 #data_response.raise_for_status()  # Check if download was successful
-df = pd.read_csv(data_url)
+df = pd.read_csv(StringIO(data_response.text))
 
 df.drop('room_id/id', axis=1, inplace=True)
 
