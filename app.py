ef67fe46f9f93b881aa1e710d40d20ccda5c6acb
Ahmed Samy <Ahmed.samy18296@gmail.com>
2024-05-15 21:09:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ data_url= "https://github.com/Ahmedsamy96/Multivariate-Time-Series/blob/main/IOT
 
 # load data from GitHub
 data_response = requests.get(data_url)
-df = pd.read_csv(data_response)
+df = pd.read_csv(data_response.content)
 
 # Drop the first column since it seems like an index
 df.drop('room_id/id', axis=1, inplace=True)
