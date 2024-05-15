81e8261c6f2b050db462ff5ed6d1da5277ee395e
Ahmed Samy <Ahmed.samy18296@gmail.com>
2024-05-15 19:26:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,17 @@ from prophet import Prophet
 from prophet.plot import add_changepoints_to_plot
 import streamlit as st
 
-def load_data(url):
-    df = pd.read_csv(url.content)
-    return df
+
 
 data_url= r"https://github.com/Ahmedsamy96/Multivariate-Time-Series/blob/main/IOT-temp.csv"
 data_response = requests.get(data_url)
 data_response.raise_for_status()  # Check if download was successful
-df = load_data(data_response)
+
+def load_data():
+    df = pd.read_csv(data_response.content)
+    return df
+    
+df = load_data()
 
 df.drop('room_id/id', axis=1, inplace=True)
 
